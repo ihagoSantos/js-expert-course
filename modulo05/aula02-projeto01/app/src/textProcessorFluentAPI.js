b8855c99260b9455a4fea6ba8fd163d178faeb50
ihagoSantos <ihagog12@gmail.com>
2025-09-19 15:24:05 -0300

    aula 02 regex

---
+++ modulo05/aula02-projeto01/app/src/textProcessorFluentAPI.js
@@ -0,0 +1,42 @@
+/**
+ * O objetivo do Fluent API é executar tarefas
+ * como um pipeline, step by step e no fim chama
+ * o build. MUITO similar ao padrão Builder.
+ * A diferença que aqui é sobre processos, o Builder
+ * é sobre construção de objetos.
+ */
+class TextProcessorFluentAPI {
+    // propriedade privada
+    #content
+    constructor(content) {
+        this.#content = content
+    }
+
+    extractPeopleData() {
+        // ?<= fala que vai extrair os dados que virão depois desse grupo
+        // [contratante|contratada] ou um ou outro, (e tem a flag no fim da expressão pra pegar maiuscula e minuscula)
+        // :\s{1} vai procurar o caracter literal de dois pontos seguindo de um espaço
+        // tudo acima fica dentro de um parenteses para falar "vamos pegar dai pra frente"
+
+        // (?!\s) negative look around, vai ignorar os contratantes no fim do documento (que só tem espaço a frente deles)
+        // .*\n pega qualquer coisa até o primeiro \n
+        //.*? non greety, esse ? faz com que ele pare na primeira recorrencia, assim ele evita ficar em loop
+
+        // $ informar que a pesquisa acaba no fim da linha
+        // g -> global
+        // m -> multiline
+        // i -> insensitive
+        const matchPerson = /(?<=[contratante|contratada]:\s{1})(?!\s)(.*\n.*?)$/gmi
+        // faz o match para encontrar a string inteira que contem os dados que precisamos
+        const onlyPerson = this.#content.match(matchPerson)
+        // console.log('onlyPerson', matchPerson.test(this.#content))
+        this.#content = onlyPerson
+        return this
+    }
+
+    build() {
+        return this.#content
+    }
+}
+
+module.exports = TextProcessorFluentAPI
