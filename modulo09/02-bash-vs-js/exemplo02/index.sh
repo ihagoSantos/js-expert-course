2753b2f1ce55ab6c9b5d8be5eaa81bc3e29283b2
Ihago Santos <ihagog12@gmail.com>
2026-01-16 07:15:19 -0300

    aula bash vs js

---
+++ modulo09/02-bash-vs-js/exemplo02/index.sh
@@ -0,0 +1,13 @@
+docker run -p "8080:80" -d nginx
+
+sleep .5
+
+curl --silent localhost:8080
+
+CONTAINER_ID=$(docker ps | grep nginx | awk '{print $1}')
+echo logs
+echo $CONTAINER_ID | xargs -I {id} docker logs {id}
+echo stop
+echo $CONTAINER_ID | xargs -I {id} docker stop {id}
+echo rm
+echo $CONTAINER_ID | xargs -I {id} docker rm {id}
